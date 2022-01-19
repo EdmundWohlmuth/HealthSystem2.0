1eaa031394be1db7ae0d0eaf007ea2bdf7331430
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-01-19 10:01:18 -0400

    added Temp HUD

--- Program.cs
+++ Program.cs
@@ -7,18 +7,31 @@ using System.Threading.Tasks;
 namespace HealthSystem2._0
 {
     class Program
-    {    
+    {
+        static bool isAlive;
+
         static void Main(string[] args)
-        {
+        {         
             PlayerCharacter playerCharacter = new PlayerCharacter();
             DrawHUD playerHUD = new DrawHUD();
 
-            playerHUD.DRAWHUD();
+            while (isAlive = true)
+            {            
+                playerCharacter.TEMPHUD();
+                playerCharacter.TakeDamage(45);
+                playerCharacter.RegenerateShield(10);
+
+                if (playerCharacter.currentLives == 0)
+                {
+                    isAlive = false;
+                }
+
+                Console.ReadKey(true);
+            }       
 
             Console.ReadKey();
 
         }
-
 
     }
 }
